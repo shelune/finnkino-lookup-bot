9bcb255179368ccf8d2a8abe7f42adac3f7dd9bd
shelune <hieu@mios.fi>
2017-06-26 16:56:00 +0300

    added xml2json parser & static fetching stuff setup

--- server.js
+++ server.js
@@ -8,14 +8,30 @@ const _ = require('lodash');
 const moment = require('moment');
 const express = require('express');
 const bodyParser = require('body-parser');
+const xmlParser = require('xml2json');
 const request = require('request-promise');
 const app = express();
 
-// Const
+// Const stuff
 const intro = 'Hello! Operator 6O at your serve. Here you can check if your favorite upcoming movie is out for schedule at Finnkino or not. You can also do some more stuff with it! Type HELP for the command you can issue!';
+
+const theaterIds = [
+  {'All': 1029},
+  {'Helsinki': 1002},
+  {'Espoo': 1012},
+  {'Vantaa': 1013},
+  {'Pori': 1019},
+  {'Tampere': 1021}
+];
+
+const baseUrl = 'http://www.finnkino.fi/XML/';
+const urlSchedules = 'Schedule';
+const urlEvents = 'Events';
+const urlDates = 'ScheduleDates';
 
 // flag checks
 let saidHello = false;
+let areaCode = ''
 
 const token = "<PAGE_ACCESS_TOKEN>";
 
@@ -93,6 +109,67 @@ function sendTextMessage(sender, text) {
 		  console.log('Error: ', response.body.error)
 	  }
   })
+}
+
+function sendEventList(sender) {
+  let messageData = {};
+  request.get({
+    uri: urlEvents,
+    baseUrl: baseUrl,
+    json: true,
+    qs: {
+      listType: 'ComingSoon',
+      area: areaCode.length == 4 ? areaCode : 1002
+    }
+  }).then(function(body) {
+    const result = body;
+    if (result.lowest_price || result.lowest_price.length == 0) {
+      price = `${itemName} \n Lowest Price: ${result.lowest_price}`;
+      itemUrl = itemUrl + _.replace(JSON.stringify(itemName), /"/g, '');
+      console.log(JSON.stringify(itemUrl))
+      // console.log(qs.stringify(itemName))
+      console.log(messageData);
+    } else {
+      messageData.text = `Couldn't find the price of that item.`;
+    }
+  }, function (error) {
+    messageData.text = `Couldn't find the price of that item.`;
+  })
+  //send the result message with prices
+  .finally(function () {
+    itemName = '';
+    let messagePriceData = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'button',
+          'text': `The price will be listed below, along with the link to detailed item view. \n --- --- --- \n ${price}`,
+          'buttons': [
+            {
+             'type': 'web_url',
+             'url': itemUrl,
+             'title': 'Check on Steam'
+            }
+          ]
+        }
+      }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messagePriceData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    });
+  });
 }
 
 // Spin up the servurrr!
