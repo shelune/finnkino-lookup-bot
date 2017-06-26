d1c8035a48427db3c3caf6fd56b00bfda72973de
shelune <hieu@mios.fi>
2017-06-26 12:38:36 +0300

    added hello to bot

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const bodyParser = require('body-parser');
 const request = require('request-promise');
 const app = express();
 
+// Const
+const intro = 'Helllo! Finnkino Lookup at your serve. Here you can check if your favorite upcoming movie is out for schedule at Finnkino or not. You can also do some more stuff with it! Type HELP for the command you can issue!';
+
+// flag checks
+let saidHello = false;
+
 const token = "<PAGE_ACCESS_TOKEN>";
 
 app.set('port', (process.env.PORT || 5000));
@@ -43,11 +49,32 @@ app.post('/webhook/', function (req, res) {
         // check first hero name
         if (event.message && event.message.text) {
           let text = event.message.text
-          sendTextMessage(sender, "Echoing: " + text);
+          if (saidHello) {
+            sendTextMessage(sender, "Echoing: " + text);
+          } else {
+            sayHello(sender);
+          }
         }
     }
     res.sendStatus(200);
 });
+
+function sayHello(sender) {
+  let messageData = {text: intro};
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+          recipient: {id: sender},
+          message: messageData,
+      }
+  }).then(function (body) {
+    saidHello = true
+  }, function (err) {
+    console.log('error encountered', err);
+  });
+}
 
 function sendTextMessage(sender, text) {
     let messageData = {text: text}
